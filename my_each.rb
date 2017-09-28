9be42d182fb6fb85715c4f988e9255c9d4cdef1a
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 19:17:13 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,17 @@
 def my_each(array)
   if block_given?
     i = 0
-    while 
-      
+    while
+      i < array.length
+      yield array[i]
+      i += 1
     end
   else
-
+    puts "There is no block given."
   end
 end
 
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
-  puts i 
+  puts i
 end
