d7e6327a5895d14af83febcd6869d59e3186a846
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 19:14:49 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+    while 
+      
+    end
+  else
+
+  end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i 
 end
